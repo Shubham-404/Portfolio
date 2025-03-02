3abe8c450686d9603b5ab8131df44d3b846d233a
Shubham <shubh.shubhamkrsingh@gmail.com>
2025-03-02 23:38:47 +0530

    Fixing deployment

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss(),
-  ],
-  base: "/Portfolio/",
-  build:{
-    outDir:"dist",
-  },
-
+  plugins: [react()],
+  base: "/Portfolio/",  // Ensures correct paths in GitHub Pages
+  build: {
+    outDir: "dist",
+    assetsDir: "assets", // Keeps CSS/JS inside `dist/assets/`
+  }
 })
