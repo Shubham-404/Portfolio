364781bab1cb4eb9ac1d1c23e1022adddac60fce
Shubham-404 <shubh.shubhamkrsingh@gmail.com>
2025-06-05 21:43:19 +0530

    updated tech stack and about me section

--- src/comps/Projects.jsx
+++ src/comps/Projects.jsx
@@ -51,4 +51,4 @@ const Projects = () => {
   )
 }
 
-export default Projects
+export default Projects 
--- src/comps/TechStack.jsx
+++ src/comps/TechStack.jsx
@@ -3,7 +3,7 @@ import gsap from 'gsap';
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 import Img from './elems/Img.jsx'
 import Heading from './elems/Heading'
-
+ 
 
 const TechStack = () => {
 
@@ -33,21 +33,23 @@ const TechStack = () => {
 
     return (
         <div id='tech-stack' className='min-h-100 w-full !p-20 max-md:!p-3 !pb-0 flex justify-center items-start'>
-            <div id='tc' className='!p-10 max-md:!p-3 max-md:max-w-120 !pb-0 w-220'>
+            <div id='tc' className='!p-10  max-md:!p-3 max-md:max-w-120 !pb-0 w-220'>
 
                 <Heading Head="Tech Stack" />
 
-                <p className='!p-5 text-lg/10 max-w-full max-lg:text-base/8 max-md:text-sm/5 h-full'>Here are some of the major tools, that I have worked with or currently working on.</p>
-                <div className=' overflow-auto'>
+                <p className='!p-5 text-lg/10 max-w-full  max-lg:text-base/8 max-md:text-sm/5 h-full'>Here are some of the major tools, that I have worked with or currently working on.</p>
+                <div className=''>
 
-                    <div className="max-w-[90vw] flex gap-5 !p-3 justify-start items-center">
-                        <Img imgAdd="tech1.png" techName="React JS" />
+                    <div className="max-w-[80vw] justify-self-center  flex flex-wrap gap-5 !p-3 !px-15 justify-center items-center">
+                        <Img imgAdd="node.png" techName="NodeJS" />
+                        <Img imgAdd="tech1.png" techName="React" />
+                        <Img imgAdd="mongo.png" techName="MongoDB" />
+                        <Img imgAdd="gsap.png" techName="GSAP" />
+                        <Img imgAdd="firebase.png" techName="Firebase" />
                         <Img imgAdd="tech2.png" techName="JavaScript" />
                         <Img imgAdd="tech3.png" techName="Git" />
-                        <Img imgAdd="tech4.png" techName="HTML" />
-                        <Img imgAdd="tech5.png" techName="CSS" />
-                        <Img imgAdd="python.png" techName="Python" />
-                        <Img imgAdd="django.png" techName="Django" />
+                        <Img imgAdd="tailwind.png" techName="TailwindCSS" />
+                        <Img imgAdd="python.png" techName="Flask" />
                     </div>
                 </div>
             </div>
